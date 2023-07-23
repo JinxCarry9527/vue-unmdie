e25c6f0eea944d19979908375b9b34301705249e
JinxCarry9527 <61592240+JinxCarry9527@users.noreply.github.com>
2023-07-24 02:34:45 +0800

    1 file added

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2020,
+  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
+  plugins: ['vue'],
+  rules: {
+    // 在这里添加项目中需要自定义的规则
+    // 示例：'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+};
